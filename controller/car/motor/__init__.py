16268d26ebd88d07c362b9f2b51c34176479bc92
Happy <oncwnuODv4nCnT1pQ88NziO_Cuk0@git.weixin.qq.com>
2024-07-23 13:08:11 +0800

    support for control PID with BROS

--- controller/car/motor/__init__.py
+++ controller/car/motor/__init__.py
@@ -1,14 +1,20 @@
 from controller import Controller
 from communicator import Communicator
 from simple_pid import PID
+from config import Config
+from logger_br import logger
 
 
 class Motor(Controller):
-    pid = PID(0.007, 0, 0, )
+    kp = Config.get("motor.kp")
+    ki = Config.get("motor.ki")
+    kd = Config.get("motor.kd")
+    pid = PID(kp, ki, kd)
 
     def __init__(self, controller_id: str, communicator: Communicator):
         super().__init__(controller_id)
         self.communicator = communicator
+        logger.info(f"Motor PID: {self.pid}")
 
     def set_speed(self, left: int, right: int):
         self.communicator.send_data({"cmd": "set_speed", "left": left, "right": right})
