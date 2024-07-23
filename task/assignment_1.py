5f0f60ad098151c622f9ef76debb82edb6eb960c
Happy <oncwnuODv4nCnT1pQ88NziO_Cuk0@git.weixin.qq.com>
2024-07-23 16:27:29 +0800

    support for config speed

--- task/assignment_1.py
+++ task/assignment_1.py
@@ -3,15 +3,23 @@ from logger_br import logger
 from task import ExecutionContext
 from .base.start_and_end import StartCommandTask, StopCommandTask
 from .base.drive_to import FollowLineUntilTurnPoint, GoStraightTask, DriveToEndTask
+from config import Config
 
 
 class CarRunOuterCircleNoUltrasound(Task):
     def __init__(self, name):
         super().__init__(name)
+        base_speed = int(Config.get('task_1.speed'))
+        max_speed_dela = int(Config.get('task_1.max_speed_delta'))
+        logger.info(f"Base speed:{base_speed} | Max speed delta: {max_speed_dela}")
+
         # 子任务列表
         self.tasks = [
             # StartCommandTask("Send Start Command"),
-            FollowLineUntilTurnPoint("Drive to Intersection", base_speed=15, max_speed_dela=8, clear_pid=False),
+            FollowLineUntilTurnPoint("Drive to Intersection",
+                                     base_speed=base_speed,
+                                     max_speed_dela=max_speed_dela,
+                                     clear_pid=False),
             GoStraightTask("Go Straight"),
             DriveToEndTask("Drive to End"),
             StopCommandTask("Send Stop Command")
